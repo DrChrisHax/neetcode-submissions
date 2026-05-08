c2600c806581bdb85751f15dcd365bee1d40cf35
Chris Manlove <cmanlove1234@outlook.com>
2026-05-07 19:30:58 -0700

    Add: evaluate-reverse-polish-notation - submission-2

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-2.cpp
@@ -0,0 +1,42 @@
+#include <cstring>
+
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+
+        std::stack<int> s;
+        
+        for (const string& token : tokens) {
+
+            if (token == "+" ||
+                token == "-"||
+                token == "*" ||
+                token == "/") {
+
+                int first = s.top();
+                s.pop();
+                int second = s.top();
+                s.pop();
+
+                switch (token[0]) {
+                    case '+':
+                        s.push(second + first);
+                        break;
+                    case '-':
+                        s.push(second - first);
+                        break;
+                    case '*':
+                        s.push(second * first);
+                        break;
+                    case '/':
+                        s.push(second / first);
+                        break;
+                } 
+            } else {
+                s.push(std::stoi(token));
+            }
+        }
+
+        return s.top();
+    }
+};
