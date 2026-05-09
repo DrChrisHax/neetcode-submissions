efbf31237130197549a70f33d2295639a99505b5
Chris Manlove <cmanlove1234@outlook.com>
2026-05-09 14:10:14 -0700

    Add: binary-search - submission-2

---
+++ Data Structures & Algorithms/binary-search/submission-2.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        
+        int l = 0;
+        int r = nums.size() - 1;
+
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+
+            int val = nums[mid];
+
+            if (val > target) {
+                r = mid - 1;
+            } else if (val < target) {
+                l = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+
+        return -1;
+    }
+};
