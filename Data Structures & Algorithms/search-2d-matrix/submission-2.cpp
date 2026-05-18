c0ede4103cc645270a35f2dcf50e0f385cb0436f
Chris Manlove <cmanlove1234@outlook.com>
2026-05-18 10:35:02 -0700

    Add: search-2d-matrix - submission-2

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-2.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        // Treat as a 1d array
+        // row = index / n
+        // col = index % n
+
+        int l = 0, r = (m * n - 1);
+
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+
+            int row = mid / n;
+            int col = mid % n;
+
+            int val = matrix[row][col];
+
+            if (val > target) {
+                r = mid - 1;
+            } else if (val < target) {
+                l = mid + 1;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+};
